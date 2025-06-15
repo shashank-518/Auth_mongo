c25d6075f8d143ff64d7ccb6bfac676193d11fa0
Shashank S <shashank5418shashu@gmail.com>
2025-06-15 23:46:20 +0530

    Logout Route

---
+++ my-app/src/app/api/Users/Logout/route.ts
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import { NextResponse } from "next/server";
+
+
+export async function GET(){
+
+    try {
+
+          const response = NextResponse.json({
+        message: "logout successfull",
+        success: true
+    })
+
+    response.cookies.set('token', '', { httpOnly: true , expires: new Date(0) })
+    
+    return response
+        
+    } catch (error) {
+
+        return NextResponse.json({
+            message: "logout failed",
+            success: false
+        })
+        
+    }
+  
+    
+}
