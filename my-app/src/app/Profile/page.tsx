ec431e1d5538e960f3b17d20d1479bfd0d09a976
Shashank S <shashank5418shashu@gmail.com>
2025-06-16 00:09:25 +0530

    logout success

--- my-app/src/app/Profile/page.tsx
+++ my-app/src/app/Profile/page.tsx
@@ -1,4 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+
 export default function Profile() {
+
+    const router = useRouter();
+    const Logout = async()=>{
+        try {
+            await axios.get("/api/Users/Logout")
+            router.push("/Login")
+
+
+        } catch (error:any) {
+            console.log(error.message);
+            
+        }
+    }
+
+
     return (
   <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 px-4">
     <div className="bg-white dark:bg-gray-800 p-8 rounded-2xl shadow-lg w-full max-w-md text-center">
@@ -8,7 +29,14 @@ export default function Profile() {
       <h2 className="text-xl font-medium text-gray-600 dark:text-gray-300">
         Profile Name
       </h2>
+        <div className="p-4">
+  <button onClick={Logout} className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-300">
+    Logout
+  </button>
+</div>
     </div>
+
+
   </div>
 );
 }
