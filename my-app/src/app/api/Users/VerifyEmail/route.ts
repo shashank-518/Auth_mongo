6b7483e120b6531d144a8403eab47ab994e4874f
Shashank S <shashank5418shashu@gmail.com>
2025-06-18 15:41:16 +0530

    Yeah

--- my-app/src/app/api/Users/VerifyEmail/route.ts
+++ my-app/src/app/api/Users/VerifyEmail/route.ts
@@ -21,7 +21,7 @@ export  async function POST(request:NextRequest){
             verifyTokenExpiry:{$gt:Date.now()},
         })
 
-        console.log(user);
+        
 
         if(!user){
             return NextResponse.json({error:"Invalid token or token expired"},{status:400})
